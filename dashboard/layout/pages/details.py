e77cc6f68cba70cf6fde92d8f135a503aa7c0185
simondac <dacosta.simon@gmail.com>
2022-05-11 21:30:08 +0200

    Add content of t1b6 to details.py

--- dashboard/layout/pages/details.py
+++ dashboard/layout/pages/details.py
@@ -1,6 +1,58 @@
-from dash import html
-from utils import card_style
+import pandas as pd
+from dash import dcc, html
+from utils import card_style, t1b6_df
 
+
+def get_company_details(df, selected_company):
+    global amount, name, hover
+
+    df = df[df['company_name']==selected_company]
+    
+    # Make lists from amount, name and hover
+    amount = list(df['emissions_category_amount'].iloc[0].split(','))
+    name = list(df['emissions_category_name'].iloc[0].split(','))
+    hover = list(df['emissions_category_hover'].iloc[0].split(','))
+
+    return df
+
+
+# Generate pie chart
+def details(selected_company):
+    df = get_company_details(t1b6_df, selected_company)
+
+    return dcc.Graph(
+            id = "details",
+            figure = {
+            "data": [
+                {
+                "values": amount,
+                "labels": name,
+                "hole": .4,
+                "type": "pie"
+    }],
+            "layout": {
+                    "title" : dict(text ="Détail des émissions",
+                                font =dict(
+                                size=20,
+                                color = 'white')),
+                    "paper_bgcolor":"#111111",
+                    "width": "2000",
+                    "annotations": [
+                        {
+                            "font": {
+                                "size": 12
+                            },
+                            "showarrow": False,
+                            "text": "",
+                            "x": 0.5,
+                            "y": 0.5
+                        }
+                    ],
+                    "showlegend": False
+                }
+            }
+    )
+    
 
 def details(selected_company):
     return html.Div(children=["Détails émissions - " + selected_company], className=card_style)
