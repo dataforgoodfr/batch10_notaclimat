7ee88601c69e57f6e1e92eb398c89c1b593df9bf
Romain Dufief <=>
2022-05-12 16:56:45 +0200

    First version of t1b4 block on dashboard

--- dashboard/layout/pages/sector_compare.py
+++ dashboard/layout/pages/sector_compare.py
@@ -1,46 +1,51 @@
 from dash import html
-from utils import card_style, t1b4_df
+from utils import t1b4_df
+
+df_t1b4 = t1b4_df
+
+printable_columns = ['company_name', 'global_score_pic', 'c1_label', 'c2_label', 'c1_color', 'c2_color']
 
 def get_sector(df, selected_company):
+    '''
+    Extracts sector for selected_company
+    '''
     index = df[df['company_name'] == selected_company].index[0]
     sector = df.loc[index, 'Sector']
     return sector
 
-def get_filtered_data(df, sector):
-    df_filtered = df[df['Sector'] == sector]
+def get_filtered_data(df, selected_company):
+    df_filtered = df[df['Sector'] == get_sector(df, selected_company)]
     return df_filtered
 
-def sector_compare(selected_company):
-    sector = get_sector(t1b4_df, selected_company)
-    sector_compare_information = get_filtered_data(t1b4_df, sector)
+def generate_html_table(df, max_rows):
+    rows = []
+    for i in range(0, max_rows):
+        image_filename = df.iloc[i]['global_score_pic']
+        rows.append(html.Tr([
+            html.Td(df.iloc[i]['company_name'], className="fw-bold"),
+            html.Td(html.Img(src=image_filename), className="me-1"),
+            html.Td('\u279c ' + df.iloc[i]['c1_label'],className="px-2",style={'color': df['c1_color'][i]}),
+            html.Td('\u279c ' + df.iloc[i]['c2_label'],className="px-2",style={'color': df['c2_color'][i]})
+            ], id=str(i),className="align-baseline"
+            ))
     return html.Div(
         children=[
-            html.Div("Comparaison Sectorielle", className="h5"),
-            html.Div([
-                html.Table([
-                    html.Tbody([
-                        html.Tr([
-                            html.Td('Entreprises', className="fw-bold"),
-                            html.Td('Réduction actuelle', className="fw-bold"),
-                            html.Td('Réduction de ses propres émissions', className="fw-bold"),
-                            html.Td('Réduction de son empreinte carbone', className="fw-bold")
-                        ]),
-                        html.Tr([
-                            html.Td(sector_compare_information['company_name'][0], className="px-2"),
-                            html.Td(html.Img(
-                                src=sector_compare_information['global_score_pic'][0]),
-                                className="me-1"
-                                ),
-                            html.Td('\u279c ' + sector_compare_information['c1_label'][0],
-                                    className="px-2",
-                                    style={'color': sector_compare_information['c1_color'][0]}),
-                            html.Td('\u279c ' + sector_compare_information['c2_label'][0],
-                                    className="px-2",
-                                    style={'color': sector_compare_information['c2_color'][0]})
-                        ],className="align-baseline"
-                        )
+        html.Div("Comparaison Sectorielle", className="h5"),
+        html.Div([
+            html.Table([
+                html.Tbody([
+                    html.Tr([
+                        html.Td('Entreprises', className="fw-bold"),
+                        html.Td('Réduction actuelle', className="fw-bold"),
+                        html.Td('Réduction de ses propres émissions', className="fw-bold"),
+                        html.Td('Réduction de son empreinte carbone', className="fw-bold")
                     ])
-                ],className="align-middle table table-borderless text-center mb-0")
-            ], className=card_style)
-        ]
-    )
+                ]),
+                html.Tbody(rows)
+            ])
+        ])
+    ])
+
+def sector_compare(selected_company):
+    sector_compare_information = get_filtered_data(df_t1b4, selected_company)[printable_columns]
+    return generate_html_table(sector_compare_information, len(sector_compare_information))
