28d447648ed6f650da83138f9922aa20e594381c
root <=>
2022-06-10 12:39:25 +0200

    debug on reduction_rate.py page + layout fixes

--- dashboard/layout/pages/reduction_rate.py
+++ dashboard/layout/pages/reduction_rate.py
@@ -1,12 +1,11 @@
 from dash import html, dcc
 import dash_bootstrap_components as dbc
-
 import plotly.graph_objects as go
 
 from utils import t2b2_df, card_style
 
 def get_filtered_data(df, selected_companies):
-    df_filtered = df[df['company_name'] == selected_companies].reset_index(drop=True)
+    df_filtered = df[df['company_name'].isin(selected_companies)].reset_index(drop=True)
     return df_filtered
 
 def get_data(df, column_name):
@@ -17,11 +16,11 @@ def get_data(df, column_name):
     return values
 
 
-def build_lollipop_chart(accomplishment, color_accomplishment, companies_names):
+def build_lollipop_chart(accomplishment, color_accomplishment, companies_names, df):
     
     fig = go.Figure()
 
-    #Create scatter race
+    #Create scatter trace
     fig.add_trace(go.Scatter(
             y = 7-accomplishment,
             x = companies_names,
@@ -42,12 +41,12 @@ def build_lollipop_chart(accomplishment, color_accomplishment, companies_names):
                     )
 
     #Add shapes
-    for i in range(len(df_filtered)):
+    for i in range(len(df)):
         fig.add_shape(type='line',
                     x0 = i,
                     y0 = 4,
                     x1 = i,
-                    y1 = (7-accomplishment)[i],
+                    y1 = (7-accomplishment[i]),
                     line=dict(
                         color='grey', 
                         width = 1,
@@ -56,7 +55,7 @@ def build_lollipop_chart(accomplishment, color_accomplishment, companies_names):
     fig.add_shape(type='line',
                 x0 = 0,
                 y0 = 4,
-                x1 = len(x)-1,
+                x1 = len(df)-1,
                 y1 = 4,
                 line=dict(
                     color='grey', 
@@ -67,8 +66,6 @@ def build_lollipop_chart(accomplishment, color_accomplishment, companies_names):
 
     #Set figure size and delete the background
     fig.update_layout(width=800, height=600,paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
-
-    fig.show()
     
     return fig
 
@@ -79,7 +76,7 @@ def chart_left(selected_companies):
     color_accomplishment = get_data(df, 'direct_score_hexa_color_code')
     companies_names = get_data(df, 'company_name')
 
-    return accomplishment, color_accomplishment, companies_names
+    return accomplishment, color_accomplishment, companies_names, df
 
 def chart_right(selected_companies):
     df = get_filtered_data(t2b2_df,selected_companies)
@@ -88,19 +85,19 @@ def chart_right(selected_companies):
     color_accomplishment = get_data(df, 'complete_score_hexa_color_code')
     companies_names = get_data(df, 'company_name')
 
-    return accomplishment, color_accomplishment, companies_names
+    return accomplishment, color_accomplishment, companies_names, df
 
 def generate_chart_left(selected_companies):
-    accomplishment, color_accomplishment, companies_names = chart_left(selected_companies)
+    accomplishment, color_accomplishment, companies_names, df = chart_left(selected_companies)
     
     return html.Div(children=[
         html.Div("Concernant leurs propres émissions", className="h5 p-3"),
         html.Div(
-        dcc.Graph(figure=build_bullet_gauge(accomplishment, color_accomplishment, companies_names),
+        dcc.Graph(figure=build_lollipop_chart(accomplishment, color_accomplishment, companies_names, df),
                     style={
-                        'width': '40%',
-                        'min-width': '40%',
-                        'max-width': '40%',
+                        'width': '100%',
+                        'min-width': '100%',
+                        'max-width': '100%',
                         'height': '100%'
                     }),
                     style={'display': 'inline-block'}),
@@ -108,16 +105,16 @@ def generate_chart_left(selected_companies):
   
 
 def generate_chart_right(selected_companies):
-    accomplishment, color_accomplishment, companies_names = chart_right(selected_companies)
+    accomplishment, color_accomplishment, companies_names, df = chart_right(selected_companies)
     
     return html.Div(children=[
         html.Div("Concernant leur empreinte carbone complète", className="h5 p-3"),
         html.Div(
-        dcc.Graph(figure=build_bullet_gauge(accomplishment, color_accomplishment, companies_names),
+        dcc.Graph(figure=build_lollipop_chart(accomplishment, color_accomplishment, companies_names, df),
                     style={
-                        'width': '40%',
-                        'min-width': '40%',
-                        'max-width': '40%',
+                        'width': '100%',
+                        'min-width': '100%',
+                        'max-width': '100%',
                         'height': '100%'
                     }),
                    style={'display': 'inline-block'}),
@@ -133,10 +130,10 @@ def reduction_rate(selected_companies):
                 dbc.Col(generate_chart_right(selected_companies), className='d-inline p-2', style={'width': '49%'}),
             ],
                     style={
-                        'width': '80%',
+                        'width': '100%',
                         'vertical-align': 'middle'
-                    })),
+                    }), className="flex-shrink-1 overflow-auto"),
         html.Hr(),
         html.Div([", ".join(selected_companies)])
     ],
-                    className=card_style)
+                    className=card_style + "flex-nowrap h-100 overflow-auto p-3")
